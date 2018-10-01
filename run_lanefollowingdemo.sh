b8e9dc85d75ed85001b7fc998120353994b4c78b
lpaull <lpaull@mit.edu>
2018-10-01 18:40:21 -0400

    adding param file name

--- run_lanefollowingdemo.sh
+++ run_lanefollowingdemo.sh
@@ -4,4 +4,4 @@ set -e
 
 source /home/software/docker/env.sh
 
-roslaunch duckietown_demos lane_following.launch line_detector_param_file_name:=$*
+roslaunch duckietown_demos lane_following.launch line_detector_param_file_name:=high_speed
